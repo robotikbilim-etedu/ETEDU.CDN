6dc38d09fb65b4a81a48719ce9fc13fc27bdc2a7
robotikbilim <60785550+robotikbilim@users.noreply.github.com>
2025-02-08 12:15:22 +0300

    First Commit

---
+++ red-m5-2-01/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "62ou4QX2ACe":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SaveActivity();
+}
+
---
+++ red-m5-2-02/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5W4cHFX6fz4":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SaveActivity();
+}
+
